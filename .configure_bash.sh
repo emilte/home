97c2a49290777fc31b9b87e5bf21d3836341bbf2
Emil Telstad <emil.telstad@gmail.com>
2022-07-28 22:32:04 +0200

    Update install.

--- .configure_bash.sh
+++ .configure_bash.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+### Imports ###
+[ -f ~/.bash_profile ] && echo "Sourcing ~/.bash_profile" && . ~/.bash_profile
+### End: Imports ###
+
 ### functions ###
 function is_yes {
     [ "$1" = "y" ] || [ "$1" = "Y" ]
@@ -21,9 +25,11 @@ function is_valid_answer {
 # https://apple.stackexchange.com/a/115373
 
 function do_action {
-    # $1: description
-    # $2: string of chained commands
-    # Utilises global 'interactive' to prompt user. Defaults to "y" if missing.
+    # $1: description.
+    # $2: string of chained commands.
+    # $3: interactive mode "y" or "n".
+    # Utilises global 'X_INTERACTIVE' to prompt user. Defaults to "y" if missing.
+    interactive = $3
     should_ask=${interactive:="y"}
 
     # Ask to do action if interactive mode.
@@ -44,20 +50,38 @@ function do_action {
 }
 
 
+# OS
+[[ "$OSTYPE" == "darwin"* ]] ; IS_MAC=$?
+[[ "$OSTYPE" == "linux-gnu"* ]] ; IS_UBUNTU=$?
+
+# Attempt to install requirements first.
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+echo ; echo ; echo ; echo "==============================================================="
+if [ $IS_UBUNTU == 0 ]; then
+    [ ! `which sudo` ] && apt install -y sudo
+    do_action "Attempt to install requirements (build-essential, procps, curl, file, git, ssh)" "sudo apt update -y ; sudo apt upgrade -y ; sudo apt install -y build-essential procps curl file git ssh" $X_INTERACTIVE
+elif [ $IS_MAC == 0 ]; then
+    do_action "Attempt to install requirements (curl, git)" "brew install git curl" $X_INTERACTIVE
+    do_action "Install xcode-select" "xcode-select --install" $X_INTERACTIVE
+fi
+
+
 # Change default terminal to bash.
 chsh -s /bin/bash
 
-# Initialise global 'interactive'.
-interactive="$1"
+# Initialise global 'X_INTERACTIVE'. Defaults to "y".
+X_INTERACTIVE=${X_INTERACTIVE:="y"}
+echo "X_INTERACTIVE=$X_INTERACTIVE"
+echo
 
 # Get user input if missing 'interactive'.
 echo
 echo
-while ! is_valid_answer $interactive ; do
-    read -p "Install with interactive mode? [y/n]: " interactive
+while ! is_valid_answer $X_INTERACTIVE ; do
+    read -p "Install with interactive mode? [y/n]: " X_INTERACTIVE
 done
 # Make lowercase.
-interactive=`echo $interactive | tr "[:upper:]" "[:lower:]"` # https://stackoverflow.com/a/2264451/12616507
+X_INTERACTIVE=`echo $X_INTERACTIVE | tr "[:upper:]" "[:lower:]"` # https://stackoverflow.com/a/2264451/12616507
 
 # Go to home directory.
 cd ~
@@ -69,7 +93,10 @@ if [ ! `which brew` ]; then
     # Non-interactive install.
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # Update PATH and current shell.
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bash_profile
+
+    # TODO: Just manually add this to bash_profile
+    # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bash_profile
+    
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 # Update brew.
@@ -118,6 +145,8 @@ do_action "Install git?" "brew install git"
 # Config already added to .bash_profile.
 # do_action "Install pyenv?" "git clone https://github.com/pyenv/pyenv.git ~/.pyenv"
 do_action "Install pyenv?" "brew install pyenv"
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
 
 ### gitlab-cli ###
 do_action "Install gitlab-cli (glab)?" "brew install glab"
