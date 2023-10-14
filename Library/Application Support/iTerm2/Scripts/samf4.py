d26c2e012b6b780bdcfce9070812e1a461a844f5
Emil Telstad <emil.telstad@gmail.com>
2023-10-14 23:53:49 +0200

    Fix previous commit.

---
+++ Library/Application Support/iTerm2/Scripts/samf4.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.7
+
+import iterm2
+from utils import new_pane, new_tab
+
+samf_red = '#a03033'
+
+
+# This script was created with the "basic" environment
+# which does not support adding dependencies with pip.
+async def main(connection):
+    # Here's a bit of example code that adds a tab to the current window:
+    app = await iterm2.async_get_app(connection)
+    window = app.current_terminal_window
+    if window is None:
+        return
+
+    # Top Left
+    top_left_pane = await new_tab(
+        window=window,
+        cmd='\n samf4 && cd frontend && yarn start \n',
+        hexa=samf_red,
+    )
+
+    # Top Right
+    top_right_pane = await new_pane(
+        session=top_left_pane,
+        vertical=True,
+        cmd='\n colima start; cd backend && dc up backend \n',
+    )
+
+    # Bottom Left
+    await new_pane(
+        session=top_left_pane,
+        cmd='y \n cd frontend \n',
+    )
+
+    # Bottom Right
+    await new_pane(
+        session=top_right_pane,
+        cmd='\n code . && cd backend \n',
+    )
+
+
+iterm2.run_until_complete(main)
---
+++ Library/Application Support/iTerm2/Scripts/utils.py
@@ -0,0 +1,46 @@
+import iterm2
+
+
+def hex_to_rgb(*, hexa: str):
+    hx = hexa.lstrip('#')
+    return tuple(int(hx[i:i + 2], 16) for i in (0, 2, 4))
+
+
+async def new_tab(
+    *,
+    window,
+    cmd: str = '',
+    hexa: str = '',
+):
+    tab = await window.async_create_tab()
+    session = tab.current_session
+    if cmd:
+        await session.async_send_text(cmd)
+    if hexa:
+        await set_color(session=session, hexa=hexa)
+    return session
+
+
+async def new_pane(
+    *,
+    session,
+    cmd: str = '',
+    vertical: bool = False,
+):
+    new_session = await session.async_split_pane(vertical=vertical)
+    if cmd:
+        await new_session.async_send_text(cmd)
+    return new_session
+
+
+async def set_color(
+    *,
+    session,
+    hexa: str,
+):
+    change = iterm2.LocalWriteOnlyProfile()
+    r, g, b = hex_to_rgb(hexa=hexa)
+    color = iterm2.Color(r, g, b)
+    change.set_tab_color(color)
+    change.set_use_tab_color(True)
+    await session.async_set_profile_properties(change)
