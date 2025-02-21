4937e3ab09ffcf25bc432eeadd51d2875f52ed84
Emil Telstad <22004178+emilte@users.noreply.github.com>
2025-02-21 16:11:33 +0100

    Sync aliases.

--- .bash_scripts/pm.sh
+++ .bash_scripts/pm.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# current_branch=`git rev-parse --abbrev-ref HEAD`
-default_branch=$(git remote show origin | awk '/HEAD branch/ {print $NF}')
-
-git checkout "$default_branch"
+git m
 git fetch --all
 git pull
 git checkout -
